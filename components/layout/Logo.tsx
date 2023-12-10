60af536184c08cd369e8ca651d067802f203bcd8
yoad208 <yoad208@gmail.com>
2023-12-10 11:52:41 +0200

    update

--- components/layout/Logo.tsx
+++ components/layout/Logo.tsx
@@ -4,10 +4,12 @@ import {cn} from "@/lib/utils";
 import {logoProps} from "@/types/layout/logoProps";
 const Logo = ({src, context, color}: logoProps) => {
     return (
-        <Link className="flex items-center pl-1 py-2 cursor-pointer" href="/">
-            <Image src={src} height={25} width={68} alt="logo"/>
-            <h2 className={cn("font-mono font-bold tracking-wider text-xl", color)}>{context}</h2>
-        </Link>
+       <div>
+           <Link className="flex w-8/12 items-center pl-1 py-2 cursor-pointer" href="/">
+               <Image src={src} height={25} width={68} alt="logo"/>
+               <h2 className={cn("font-mono font-bold tracking-wider text-xl", color)}>{context}</h2>
+           </Link>
+       </div>
     )
 }
 
